952a023cf0d4a96d63d7edc17a852565746910d8
duck_alan <duckalan2@gmail.com>
2025-01-03 13:20:54 +0300

    Finished home page api

--- CourseworkTerm5.Server/ApiEndpointGroup.cs
+++ CourseworkTerm5.Server/ApiEndpointGroup.cs
@@ -7,7 +7,8 @@ namespace CourseworkTerm5.Server
         public static RouteGroupBuilder MapApi(this RouteGroupBuilder group)
         {
             group.MapGet("/product-categories", async (TastyPizzaContext dbContext) =>
-                await dbContext.ProductCategories
+                await dbContext
+                    .ProductCategories
                     .AsNoTracking()
                     .Include(pc => pc.Products)
                     .Select(pc => new
@@ -24,6 +25,49 @@ namespace CourseworkTerm5.Server
                         })
                     })
                     .ToListAsync()
+            );
+
+            // TODO: Add check for empty Variants field if passed non pizza productId
+            group.MapGet("/pizzas/{productId}", async (TastyPizzaContext dbContext, int productId) =>
+                await dbContext
+                    .Products
+                    .AsNoTracking()
+                    .Where(p => p.ProductId == productId)
+                    .Include(p => p.PizzaVariants)
+                    .ThenInclude(pv => pv.PizzaDiameter)
+                    .Select(p => new
+                    {
+                        p.Name,
+                        p.Description,
+                        p.ImageUrl,
+                        Variants = p.PizzaVariants.Select(pv => new
+                        {
+                            pv.PizzaDiameter.DiameterCm,
+                            pv.WeightGram,
+                            pv.PriceRub
+                        })
+                    })
+                    .FirstAsync()
+            );
+
+            group.MapGet("/toppings", async (TastyPizzaContext dbContext) =>
+                await dbContext
+                .Toppings
+                .AsNoTracking()
+                .Include(t => t.ToppingVariants)
+                .ThenInclude(tv => tv.PizzaDiameter)
+                .Select(t => new
+                {
+                    t.ToppingId,
+                    t.Name,
+                    t.ImageUrl,
+                    Variants = t.ToppingVariants.Select(tv => new
+                    {
+                        tv.PizzaDiameter.DiameterCm,
+                        tv.WeightGram,
+                        tv.PriceRub,
+                    })
+                }).ToListAsync()
             );
 
             return group;
